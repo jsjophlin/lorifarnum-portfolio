3343b2e3b0a9b19df14c88c547c1de875d3d5fa8
Josiah Jophlin <jsjophlin@gmail.com>
2020-02-01 22:02:17 -0500

    Save progress.

--- src/components/project.js
+++ src/components/project.js
@@ -2,14 +2,9 @@ import React from "react"
 import PropTypes from "prop-types"
 import SbEditable from "storyblok-react"
 import Components from "./components.js"
-import useCategories from "./staticQueries/categories"
 
 const Project = ({ blok, isTeaser }) => {
   const { description, image, name, services } = blok
-  const categories = useCategories()
-
-  const getCategories = key =>
-    categories.filter(({ node }) => node.uuid === key)[0].node
 
   return (
     <SbEditable content={blok}>
@@ -18,23 +13,6 @@ const Project = ({ blok, isTeaser }) => {
           <div className="project-meta">
             <h2>{name}</h2>
             <h3>{description}</h3>
-            <h4>Services:</h4>
-            {services && (
-              <ul>
-                {services.map(key => {
-                  const {
-                    content,
-                    field_image_string,
-                    full_slug,
-                    name,
-                    slug,
-                    uuid,
-                  } = getCategories(key)
-
-                  return <li key={uuid}>{name}</li>
-                })}
-              </ul>
-            )}
           </div>
         )}
         {image &&
