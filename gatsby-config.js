cc58a993402150a8601bce4a17dba97a85026497
Josiah Jophlin <jsjophlin@gmail.com>
2020-02-06 11:11:46 -0500

    Only purging tailwinds css.

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,7 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         tailwind: true, // Enable tailwindcss support
       },
+      purgeOnly: ["/src/styles/global.css"], // Purge only Tailwinds css
     },
     {
       resolve: `gatsby-plugin-manifest`,
