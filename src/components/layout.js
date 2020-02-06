f87b56f2948e4b56e0b2dd321f03ed7c2d64d7fa
Josiah Jophlin <jsjophlin@gmail.com>
2020-02-06 15:19:22 -0500

    Updated layout query name.

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,7 @@ import "../styles/theme.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
+    query SiteMetaQuery {
       site {
         siteMetadata {
           title
