c6e94c7cb7d93eed3cd755983c864d4339f7f78e
Josiah Jophlin <jsjophlin@gmail.com>
2020-02-07 10:26:55 -0500

    Replaced Masonry library with react-photo-gallery for tile functionality.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from "react"
 import SbEditable from "storyblok-react"
 import cn from "classnames"
 import Helmet from "react-helmet"
+import Gallery from "react-photo-gallery"
 import Components from "./Components.js"
 import Layout from "./Layout"
 import useProjects from "./staticQueries/projects"
@@ -9,6 +10,28 @@ import "../styles/masonry.css"
 
 const Home = ({ blok, seo }) => {
   const projects = useProjects()
+  const photos = []
+
+  projects.forEach(({ node }) => {
+    const { name, slug, uuid } = node
+    const content = JSON.parse(node.content)
+    const title = content.name
+    const { _uid, image, dimensions, description } = content
+    const thumbnail = image[0].image
+    const re = /.+?(?=x)/
+    const width = parseInt(re.exec(dimensions)[0])
+    const height = parseInt(dimensions.substr(dimensions.indexOf("x") + 1))
+
+    photos.push({
+      src: thumbnail,
+      width: width,
+      height: height,
+    })
+  })
+
+  const handleClick = e => {
+    console.log(e.currentTarget)
+  }
 
   return (
     <SbEditable content={blok}>
@@ -16,7 +39,8 @@ const Home = ({ blok, seo }) => {
         <body className="home" />
       </Helmet>
       <Layout>
-        {projects ? (
+        <Gallery photos={photos} direction={"column"} onClick={handleClick} />
+        {/* {projects ? (
           <div className="grid">
             {projects.map(({ node }) => {
               const { slug, uuid } = node
@@ -43,7 +67,7 @@ const Home = ({ blok, seo }) => {
           </div>
         ) : (
           <div>No projects available to list.</div>
-        )}
+        )} */}
       </Layout>
     </SbEditable>
   )
