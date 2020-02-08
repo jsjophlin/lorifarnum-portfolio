f4c1ebd3bd2d40c592d5b0a946287290682a708e
Josiah Jophlin <jsjophlin@gmail.com>
2020-02-08 13:07:54 -0500

    Swapped bg-image with darker version.

--- src/components/staticQueries/projectBgImage.js
+++ src/components/staticQueries/projectBgImage.js
@@ -4,7 +4,7 @@ const useProjectBgImage = () => {
   const { file } = useStaticQuery(
     graphql`
       query PROJECT_BG_IMAGE_QUERY {
-        file(relativePath: { eq: "project-bg.png" }) {
+        file(relativePath: { eq: "project-bg-dark.png" }) {
           childImageSharp {
             fluid(maxWidth: 1350, quality: 100) {
               ...GatsbyImageSharpFluid_withWebp
