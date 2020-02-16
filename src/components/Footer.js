7f372c4326f18850077c4e465187d0b6a56186fd
Josiah Jophlin <jsjophlin@gmail.com>
2020-02-15 21:18:51 -0500

    Updated footer links.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,57 @@
 import React from "react"
+import cn from "classnames"
+import styles from "./Footer.module.css"
+
+const date = new Date()
+const year = date.getFullYear()
 
 const Footer = () => (
-  <footer className="flex flex-col md:flex-row md:justify-between mb-4">
+  <footer
+    className={cn(
+      styles.footer,
+      "flex flex-col md:flex-row md:justify-between mb-4"
+    )}
+  >
     <ul className="flex flex-wrap mb-4 md:mb-0">
       <li className="mr-4">
-        <a href="https://www.dribble.com">Dribble</a>
+        <a
+          className="hover:text-brandBlue"
+          href="https://dribbble.com/lorifarnumdesign"
+        >
+          Dribble
+        </a>
       </li>
       <li className="mr-4">
-        <a href="https://www.instagram.com">Instagram</a>
+        <a
+          className="hover:text-brandBlue"
+          href="https://instagram/lorifarnumdesign"
+        >
+          Instagram
+        </a>
       </li>
       <li className="mr-4">
-        <a href="https://www.facebook.com">Facebook</a>
+        <a
+          className="hover:text-brandBlue"
+          href="https://facebook.com/lorifarnumdesign"
+        >
+          Facebook
+        </a>
       </li>
       <li className="mr-4">
-        <a href="mailto:lori@lorifarnum.com">lori@lorifarnum.com</a>
+        <a
+          className="hover:text-brandBlue"
+          href="https://www.linkedin.com/in/lorifarnum/"
+        >
+          LinkedIn
+        </a>
+      </li>
+      <li className="mr-4">
+        <a className="hover:text-brandBlue" href="mailto:lori@lorifarnum.com">
+          lori@lorifarnum.com
+        </a>
       </li>
     </ul>
-    <div className="text-body text-base">All Content © Lori Farnum</div>
+    <div className="text-body text-base">{`All Content © ${year} Lori Farnum`}</div>
   </footer>
 )
 
